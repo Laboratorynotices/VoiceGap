44160a39293b49ae040a8a688c2001ac70f554e0
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-03-13 17:46:00 +0100

    feat(layout): обновлена структура сетки с использованием CSS Grid
    - Добавлена новая HTML-структура в `src/App.vue` с классами Tailwind CSS
    - Настроен родительский контейнер `#app` в `src/style.css` с `grid-cols`
    - Упорядочены элементы с помощью `order-last` для SEO-оптимизации
    - Улучшена адаптивность за счёт префиксов `lg:` и `2xl:`

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline border-2 rounded-xl">Hello world!</h1>
+  <div class="border-1 lg:col-span-2 2xl:col-span-5 order-last">
+    Описание приложения
+  </div>
+  <div class="border-1 lg:col-span-2 2xl:col-span-5">Шапка</div>
+  <div class="border-1 2xl:col-span-2 2xl:row-span-2">Поле ввода</div>
+  <div class="border-1 2xl:col-span-2 2xl:row-span-2">Вывод результата</div>
+  <div class="border-1 lg:col-span-2 2xl:col-span-1">Статистика</div>
+  <div class="border-1 lg:col-span-2 2xl:col-span-1">Настройки</div>
 </template>
