055e8ce5b442ee342c0de0e18e1c70d14f2cb9a0
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-03-14 13:18:41 +0100

    feat(vite): настройка base и outDir для GitHub Pages
    Добавил в `vite.config.ts` параметры:
    - `base: "/VoiceGap/"` — для корректного пути на GitHub Pages
    - `outDir: "docs"` — чтобы сборка размещалась в папке `docs`
    
    Также выполнил сборку с обновленными настройками.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  // Имя репозитория на GitHub
+  base: "/VoiceGap/",
+  build: {
+    // Путь, куда будет генерироваться сборка,
+    // а на GitHub Pages это будет папка docs
+    outDir: "docs",
+  },
 });
